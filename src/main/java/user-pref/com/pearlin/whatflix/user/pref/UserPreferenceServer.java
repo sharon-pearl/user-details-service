f287ad35ae61b5ee4ce257810de2af57ab5fa955
pearlin-sargunar <p.sargunar@everteam.com>
2018-04-21 15:17:03 +0530

    initial commit

---
+++ src/main/java/user-pref/com/pearlin/whatflix/user/pref/UserPreferenceServer.java
@@ -0,0 +1,11 @@
+package com.pearlin.whatflix.user.pref;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPreferenceServer {
+
+	public static void main(String...args) {
+		System.out.println("Starting User Preferences Server");
+	}
+}
---
+++ src/main/java/user-pref/module-info.java
@@ -0,0 +1,5 @@
+module userpreferences{
+	requires java.base;
+	requires spring.boot;
+	exports com.pearlin.whatflix.user.pref;
+}
